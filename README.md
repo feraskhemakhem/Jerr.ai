a683f526b2ac2636b1debed50feeda24309a4c45
feraskhemakhem <feraskhemakhem@gmail.com>
2021-07-09 20:53:01 +0100

    added parrot command

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # Jerr.ai
 A discord bot that acts as my personal assistant in keeping track of other bot updates, assigning roles based on reaction, etc.
 
-For now, the invite link is: https://discord.com/oauth2/authorize?client_id=863095857335369728&scope=bot&permissions=3154627648
+For now, the invite link is (this)[https://discord.com/oauth2/authorize?client_id=863095857335369728&scope=bot&permissions=3154627648]
 
 Permissions include:
 - Manage roles
--- bot.js
+++ bot.js
@@ -6,22 +6,41 @@ dotenv.config(); //https://coderrocketfuel.com/article/how-to-load-environment-v
 client.debug = true;
 client.prefix = '!';
 
+const test_server_id = '625862970135805983';
+
+// for v1.0.0
+// - Add roles based on reactions
+// - Check github for updates on list of repos
+
 // ready event callback
-client.once('ready', () => {
-    if (!client.debug) {
-        client.guilds.fetch('625862970135805983')
-            .then(guild => guild.systemChannel.send(`@everyone Hey! Sorry I'm a little late to the party. What's the deal with parties, anyway? *queues laugh track*`))   ;
-    }
+client.once('ready', async () => {
+    // set bot status
+    client.user.setActivity(`ya like jazz?`, {type: 'PLAYING'});
+
+    // wait for a reference to author's user to save
+    const app = await client.fetchApplication();
+    client.my_maker = app.owner;
+
+    // queue that you're ready
     console.log(`les go`);
 });
 
-// // message event callback
-// client.on('message', message => {
-//     // if it starts with prefix, process
-//     if (message.startsWith(client.prefix)) {
+// message event callback
+client.on('message', async message => {
+    // ignore all messages unless it is from the creator himself
+    if (message.author.id !== client.my_maker.id) return;
 
-//     }
-// });
+    // if it starts with prefix, process
+    if (message.content.startsWith(client.prefix)) {
+
+        // parrot for telling you what to say
+        if (message.content.startsWith('!parrot')) {
+            const reply = message.content.substring(message.content.indexOf(' '));
+            client.guilds.fetch(test_server_id)
+                .then(guild => guild.systemChannel.send(reply));
+        }
+    }
+});
 
 
 
